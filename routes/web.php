17396bd8d9a55f49eee98fac2dc9dc4e88cb43e5
RadMa <13coolguy37@gmail.com>
2024-11-16 22:21:52 +0500

    Integrations implemented

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,32 @@
 <?php
 
-use App\Http\Controllers\EmployeeController;
-use App\Http\Controllers\PdfController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Telegram\Bot\Laravel\Facades\Telegram;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/userform', [\App\Http\Controllers\EmployeeController::class, 'userform']);
-Route::post('/store_form', [\App\Http\Controllers\EmployeeController::class, 'store'])->name('store-user');
-Route::post('/store_form_json', [\App\Http\Controllers\EmployeeController::class, 'storeJson']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-
-Route::get('/user/{id}', [EmployeeController::class, 'get']);
-
-Route::get('/user', [EmployeeController::class, 'index']);
-Route::get('/resume/{id}', [PdfController::class, 'index']);
-
-Route::get('/logs', function(){
-    return view('logs');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
+
+Route::get('test-telegram', function () {
+    Telegram::sendMessage([
+    'chat_id' => env('TELEGRAM_CHANNEL_ID', ''),
+    'parse_mode' => 'html',
+    'text' => 'Произошло тестовое событие'
+    ]);
+    return response()->json([
+    'status' => 'success'
+    ]);
+    });
